9ee031d7413a5c03c0e3f8af692b75ac391c7a87
dmanoharan <40506501+dmanoharan@users.noreply.github.com>
2018-06-25 15:46:16 -0700

    Update HelloWildFlyController.java

--- src/main/java/bootwildfly/HelloWildFlyController.java
+++ src/main/java/bootwildfly/HelloWildFlyController.java
@@ -3,10 +3,7 @@ package bootwildfly;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+
 @RestController
 public class HelloWildFlyController {
 
